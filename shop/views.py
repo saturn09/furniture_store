437cb3886f700d9a25ff0e78ba77763d2b113da7
saturn09 <nurtas977@gmail.com>
2018-05-19 14:13:45 +0600

    Initial commit

---
+++ shop/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.views.generic import TemplateView
+
+
+class Index(TemplateView):
+    template_name = '../templates/shop/index.html'
