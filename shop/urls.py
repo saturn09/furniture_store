d1fceaa71939e7a4ae82ca1215637fb945ef3e26
nabdreimov <nabdreimov@wooppay.com>
2018-05-21 17:35:53 +0600

    add: names of endpoints

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'shop'
 urlpatterns = [
-    path('index/', views.Index.as_view()),
+    path('index/', views.IndexView.as_view(), name='index'),
+    path('store/', views.ShopView.as_view(), name='store')
 ]
