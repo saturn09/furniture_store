82caab805e5b0d27b91187b7ca2cf736e7411994
nabdreimov <nabdreimov@wooppay.com>
2018-05-21 17:36:39 +0600

    add: ShopView and detailed FurnitureView

--- shop/views.py
+++ shop/views.py
@@ -3,8 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 
-from django.views.generic import TemplateView
+from django.views import generic
 
 
-class Index(TemplateView):
+class IndexView(generic.TemplateView):
     template_name = '../templates/shop/index.html'
+
+
+class ShopView(generic.TemplateView):
+    template_name = '../templates/shop/shop.html'
+
+
+class FurnitureView(generic.DetailView):
+    pass
